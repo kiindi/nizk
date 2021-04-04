65735ae54147449d5796c5d187b6a395e9132dba
Nizk Source <76686804+kiindi@users.noreply.github.com>
2021-04-04 10:14:09 +0200

    v1

--- YEMEN/Run.lua
+++ YEMEN/Run.lua
@@ -54,7 +54,7 @@ local DirFol = io.popen("echo $(cd $(dirname $0); pwd)"):read('*all'):gsub(' ','
 user = {}
 user.username = SUDO_USER
 user.Source  = "/root/NK"
-local url , res = https.request('https://kiindi-api.ml/lua/nizk/elnizk.php?token='..Token..')
+local url , res = https.request('https://kiindi-api.ml/lua/nizk/elnizk.php?token='..Token..'')
 print(res)
 if res ~= 200 then
 print('\n\27[1;31m￤ Conect is Failed !\n￤ حدث خطـآ في آلآتصـآل بآلسـيرفر , يرجى مـرآسـلهہ‏‏ مـطـور آلسـورس ليتمـگن مـن حل آلمـشـگلهہ‏‏ في آسـرع وقت مـمـگن . !')
